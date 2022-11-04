f7314e19d7c50e3870b9713a0a19aa337620da14
Pitrified <nobilipietro@gmail.com>
2022-11-05 00:22:43 +0100

    Add some basic UI structure

--- internal/gui/app.go
+++ internal/gui/app.go
@@ -6,6 +6,7 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -13,6 +14,9 @@ type guiApp struct {
 	c *guiController
 
 	kb *keyboard
+
+	word    *widget.Label
+	glosses *widget.Label
 
 	fyneApp fyne.App
 	mainWin fyne.Window
@@ -63,16 +67,40 @@ func (a *guiApp) typedKey(ev *fyne.KeyEvent) {
 func (a *guiApp) buildUI() {
 
 	// ##### KEYBOARD #####
-
 	a.kb = newKeyboard(a)
 	contKeyboard := a.kb.buildKeyboard()
 
 	// ##### MAIN SCREEN #####
-	contMain := widget.NewLabel("Mock")
+	// word to find
+	a.word = widget.NewLabel("Word: .... ... ... .. .. . . (4)")
+	a.word.Alignment = fyne.TextAlignCenter
+	// meaning of the word
+	a.glosses = widget.NewLabel("Glosses:\none\ntwo\na very long gloss that explains a lot of information about the word")
+	a.glosses.Wrapping = fyne.TextWrapWord
+	// assemble word info
+	contWord := container.NewVBox(
+		layout.NewSpacer(),
+		a.word,
+		a.glosses,
+		layout.NewSpacer(),
+	)
+	// add more elements to the screen
+	labelControl := widget.NewLabel("Mock for buttons: hint, next...")
+	labelControl.Alignment = fyne.TextAlignCenter
+	// build the main screen
+	contMain := container.NewBorder(
+		nil, labelControl, nil, nil,
+		contWord,
+	)
 
 	// ##### ASSEMBLE #####
-	borderCont := container.NewBorder(nil, contKeyboard, nil, nil,
-		container.NewCenter(contMain),
+	// frankly this title is useless, meh
+	labelTitle := widget.NewLabelWithStyle(
+		"Accenter", fyne.TextAlignCenter, fyne.TextStyle{Bold: true},
+	)
+	borderCont := container.NewBorder(labelTitle, contKeyboard, nil, nil,
+		// container.NewCenter(contMain),
+		container.NewPadded(contMain),
 	)
 	a.mainWin.SetContent(borderCont)
 
