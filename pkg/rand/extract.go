27c1b8a42c7004bce8fb1e0e57bda757fa4973e4
Pitrified <nobilipietro@gmail.com>
2022-12-08 10:30:03 +0100

    Let totalWeight be computed on the fly

--- pkg/rand/extract.go
+++ pkg/rand/extract.go
@@ -19,9 +19,18 @@ func Pick[K comparable, V any](m map[K]V) K {
 }
 
 // Pick a random key in a map, according to the weights returned by getWeight.
+//
+// The weights must sum to totalWeight, set it to 0 to compute it.
 func PickMap[K comparable, V any](
 	m map[K]V, getWeight func(V) int, totalWeight int,
 ) K {
+	// compute the total if we don't have it
+	if totalWeight == 0 {
+		for _, v := range m {
+			totalWeight += getWeight(v)
+		}
+	}
+	// pick a random number and find in which interval it falls
 	i := rand.Intn(totalWeight)
 	for k, v := range m {
 		i -= getWeight(v)
